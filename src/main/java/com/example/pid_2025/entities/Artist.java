7cfebcf97a3d381b053cb8e9a848dc06a72f02aa
navil <navil1228@gmail.com>
2025-04-23 23:37:44 +0200

    add class Tag,type and more methods for user and Type

--- src/main/java/com/example/pid_2025/entities/Artist.java
+++ src/main/java/com/example/pid_2025/entities/Artist.java
@@ -1,26 +1,54 @@
 package com.example.pid_2025.entities;
 
 import jakarta.persistence.*;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 @ToString
 @Entity
-@Table(name = "artistsstat")
+@Table(name = "artists")
 public class Artist {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstname;
     private String lastname;
+    @ManyToMany(mappedBy = "artists")
+    @JsonIgnore
+    private  List<Type> types = new ArrayList<>();
+
 
     public Artist(String firstname, String lastname) {
         this.firstname = firstname;
         this.lastname = lastname;
     }
+    public List<Type> getTypes() {
+        return types;
+    }
+
+    public Artist addType(Type type) {
+        if (!this.types.contains(type)) {
+            this.types.add(type);
+            type.addArtist(this);
+        }
+        return this;
+    }
+
+    public Artist removeType(Type type) {
+        if (this.types.contains(type)) {
+            this.types.remove(type);
+            type.getArtists().remove(this);
+        }
+        return this;
+    }
+
 
     @Override
     public String toString() {
--- src/main/java/com/example/pid_2025/entities/User.java
+++ src/main/java/com/example/pid_2025/entities/User.java
@@ -1,16 +1,17 @@
 package com.example.pid_2025.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 
 @Entity
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
 @ToString
 public class User {
@@ -28,7 +29,54 @@ public class User {
     @OneToMany
     private List<Role> roles ;
 
+    public void addRole(Role role) {
+    }
 
 
+    @Entity
+    @Data
+    @NoArgsConstructor
 
+    @Table(name = "types")
+
+    public static class Type {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
+        private Long id;
+
+        @NotBlank(message = "Le type ne peut pas être vide")
+        private String type;
+
+        @ManyToMany
+        @JoinTable(
+                name = "artist_type",
+                joinColumns = @JoinColumn(name = "type_id"),
+                inverseJoinColumns = @JoinColumn(name = "artist_id"))
+        private List<Artist> artists = new ArrayList<>();
+
+        public Type(String type) {
+            this.type = type;
+        }
+
+        public com.example.pid_2025.entities.Type addArtist(Artist artist) {
+            if (!this.artists.contains(artist)) {
+                this.artists.add(artist);
+                artist.getTypes().add(this);
+            }
+            return this;
+        }
+
+        public com.example.pid_2025.entities.Type removeArtist(Artist artist) {
+            if (this.artists.contains(artist)) {
+                this.artists.remove(artist);
+                artist.getTypes().remove(this);
+            }
+            return this;
+        }
+
+        @Override
+        public String toString() {
+            return "Type [id=" + id + ", type=" + type + "]";
+        }
+    }
 }
