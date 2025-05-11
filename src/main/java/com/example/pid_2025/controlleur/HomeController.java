9ec86947a07d99511abac8c59202ab72f0ddb21c
Nabil Zaoui <navil1228@gmail.com>
2025-05-11 23:41:23 +0200

    Update Home and homeController And Mapping

--- src/main/java/com/example/pid_2025/controlleur/HomeController.java
+++ src/main/java/com/example/pid_2025/controlleur/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/")
+    @GetMapping("/home")
     public String home() {
         return "layouts/home"; // Cette vue doit correspondre à un fichier home.html dans le dossier templates
     }
